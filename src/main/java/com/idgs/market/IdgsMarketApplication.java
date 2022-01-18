ac23fbe5f94dee042be51dfd5098a55a6b16942d
VictorTamayoBriones <victortamayo509@gmail.com>
2022-01-18 11:14:43 -0600

    Initial commit

---
+++ src/main/java/com/idgs/market/IdgsMarketApplication.java
@@ -0,0 +1,13 @@
+package com.idgs.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdgsMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdgsMarketApplication.class, args);
+	}
+
+}
